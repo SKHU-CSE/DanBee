15b7879cfc7e29fb6ce463fe86b21fa78d064647
namsoo <are34@naver.com>
2019-05-06 20:26:21 +0900

    main버튼 구현

--- app/src/main/java/danbee/com/MainActivity.java
+++ app/src/main/java/danbee/com/MainActivity.java
@@ -1,5 +1,7 @@
 package danbee.com;
 
+import android.content.Intent;
+import android.graphics.Rect;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -18,12 +20,16 @@ import com.naver.maps.map.overlay.LocationOverlay;
 import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.overlay.Overlay;
 import com.naver.maps.map.util.FusedLocationSource;
+import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
+import com.nightonke.boommenu.BoomButtons.TextInsideCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     Marker marker;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
     private FusedLocationSource locationSource;
 
+    BoomMenuButton boombt;
 
     boolean startPos = true;
     @Override
@@ -40,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
 
         locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+        boomMenuSet();
     }
 
     @Override
@@ -127,5 +134,108 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng southEast = new LatLng(44.35, 132);
         naverMap.setExtent(new LatLngBounds(northWest, southEast));
 
+    }
+
+    //버튼메뉴생성
+    void boomMenuSet(){
+        boombt = findViewById(R.id.bmb);
+        for (int i = 0; i < boombt.getPiecePlaceEnum().pieceNumber(); i++) {
+            TextInsideCircleButton.Builder builder = new TextInsideCircleButton.Builder()
+                    .normalImageRes(buttonImage(i))
+                    .normalText(buttonText(i))
+                    .listener(new OnBMClickListener() {
+                        @Override
+                        public void onBoomButtonClick(int index) {
+                            btClickIntent(index);
+                        }
+                    })
+                    .imagePadding(new Rect(0,0,0,30));
+            boombt.addBuilder(builder);
+        }
+    }
+
+    //버튼이미지생성
+    int buttonImage(int pos){
+        int res = -1;
+        switch (pos){
+            case 0:
+                res=R.drawable.danbeelogo;
+                break;
+            case 1:
+                res=R.drawable.danbeelogo;
+                break;
+            case 2:
+                res=R.drawable.danbeelogo;
+                break;
+            case 3:
+                res=R.drawable.danbeelogo;
+                break;
+            case 4:
+                res=R.drawable.danbeelogo;
+                break;
+            case 5:
+                res=R.drawable.danbeelogo;
+                break;
+            case 6:
+                res=R.drawable.danbeelogo;
+                break;
+        }
+        return res;
+    }
+
+    //버튼텍스트 생성
+    String buttonText(int pos){
+        String title="";
+        switch (pos){
+            case 0:
+                title="QR코드";
+                break;
+            case 1:
+                title="로그인";
+                break;
+            case 2:
+                title="내정보";
+                break;
+            case 3:
+                title="";
+                break;
+            case 4:
+                title="공지사항/Q&A";
+                break;
+            case 5:
+                title="이용내역";
+                break;
+            case 6:
+                title="사용방법";
+                break;
+        }
+        return title;
+    }
+
+    void btClickIntent(int index){
+        switch (index){
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+                Intent intent = new Intent(this, NoticeActivity.class);
+                startActivity(intent);
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+        }
     }
 }
