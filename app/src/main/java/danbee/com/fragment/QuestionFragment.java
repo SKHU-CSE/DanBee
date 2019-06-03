d6d6afac609f2ddc7d65c8d227157b4367a4bddd
namsoo <are34@naver.com>
2019-06-04 00:02:00 +0900

    expandLayout 구현

--- app/src/main/java/danbee/com/fragment/QuestionFragment.java
+++ app/src/main/java/danbee/com/fragment/QuestionFragment.java
@@ -7,16 +7,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import net.cachapa.expandablelayout.ExpandableLayout;
 
 import danbee.com.R;
 
 
 public class QuestionFragment extends Fragment {
-
+    ExpandableLayout expandview;
+    ImageView arrow;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_question, container, false);
+        expandview = rootView.findViewById(R.id.question_fragment_expandable_layout);
+        arrow = rootView.findViewById(R.id.question_fragment_down);
+
+        rootView.findViewById(R.id.question_fragment_expand_tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(expandview.isExpanded()){
+                    expandview.collapse();
+                    arrow.setImageResource(R.drawable.downarrow);
+                }else {
+                    expandview.expand();
+                    arrow.setImageResource(R.drawable.uparrow);
+                }
+            }
+        });
+
+
+
+
 
         return rootView;
     }
