5e34bdf50824a5de9dff9304967f03da0b24faf2
jaehee6 <0jujh8@gmail.com>
2019-06-23 14:53:41 +0900

    뷰페이저 추가

--- app/src/main/java/danbee/com/GuideActivity.java
+++ app/src/main/java/danbee/com/GuideActivity.java
@@ -1,8 +1,10 @@
 package danbee.com;
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +13,9 @@ import java.util.ArrayList;
 
 import danbee.com.fragment.GuideFragment1;
 import danbee.com.fragment.GuideFragment2;
+import danbee.com.fragment.GuideFragment3;
+import danbee.com.fragment.GuideFragment4;
+import danbee.com.fragment.GuideFragment5;
 
 public class GuideActivity extends AppCompatActivity {
 
@@ -21,16 +26,24 @@ public class GuideActivity extends AppCompatActivity {
 
         ViewPager pager = findViewById(R.id.pager);
         pager.setOffscreenPageLimit(3); //뒤에안보이는화면 캐싱
-        pager.setClipToPadding(false);  //자식들의 패딩무시 (양쪽미리보기하기위해서)
-        pager.setPadding(150,0,150,0);
+        //pager.setClipToPadding(false);  //자식들의 패딩무시 (양쪽미리보기하기위해서)
+        pager.setPadding(100,30,100,30);
 
         GuidePagerAdapter adapter = new GuidePagerAdapter(getSupportFragmentManager());
         GuideFragment1 guideFragment1 = new GuideFragment1();
         GuideFragment2 guideFragment2 = new GuideFragment2();
+        GuideFragment3 guideFragment3 = new GuideFragment3();
+        GuideFragment4 guideFragment4 = new GuideFragment4();
+        GuideFragment5 guideFragment5 = new GuideFragment5();
+
         adapter.addItem(guideFragment1);
         adapter.addItem(guideFragment2);
+        adapter.addItem(guideFragment3);
+        adapter.addItem(guideFragment4);
+        adapter.addItem(guideFragment5);
 
         pager.setAdapter(adapter);
+
 
     }
 
